a46aae1dd513bc268797d0a8b9d994d8ac172ecf
uzervlad <vladik0701@gmail.com>
2019-11-01 16:09:57 +0300

    Fix Eval

--- src/modules/Admin/Eval.ts
+++ src/modules/Admin/Eval.ts
@@ -6,7 +6,7 @@ export default class EvalCommand extends Command {
         super(["eval"], module, async (ctx, self, args) => {
             if(ctx.senderId != module.bot.config.vk.owner) return;
 
-            eval(args.string.join(" "));
+            eval(args.full.join(" "));
         });
     }
 }
