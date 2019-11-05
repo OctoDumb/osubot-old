9bb1a9f1426026298cd542f831f7208f74bc674a
uzervlad <vladik0701@gmail.com>
2019-11-05 17:48:05 +0300

    map: support all mods

--- src/templates/PP.ts
+++ src/templates/PP.ts
@@ -4,20 +4,47 @@ import Mods from "../pp/Mods";
 import Util from "../Util";
 
 export default function(map: APIBeatmap, args: ICommandArgs): string {
-    if(map.mode != 0) return '';
     let calc = new BanchoPP(map, new Mods(args.mods));
     let hits = map.objects.circles + map.objects.sliders + map.objects.spinners;
-    let ppArgs = Util.createPPArgs({
-        acc: args.acc / 100 || 1,
-        combo: args.combo ? Math.min(map.combo, Math.max(1, args.combo)) : map.combo,
-        hits,
-        miss: args.miss ? Math.min(hits, Math.max(0, args.miss)) : 0,
-        mods: new Mods(args.mods)
-    }, map.mode)
-    let pp = calc.calculate(ppArgs);
-    return `${map.artist} - ${map.title} [${map.version}] by ${map.creator.nickname}
+    if(map.mode == 1)
+        hits -= map.objects.sliders;
+    if(map.mode == 1 || map.mode == 3)
+        hits -= map.objects.spinners;
+    switch(map.mode) {
+        case 0:
+        case 1:
+        case 2: {
+            let ppArgs = Util.createPPArgs({
+                acc: args.acc / 100 || 1,
+                combo: args.combo ? Math.min(map.combo, Math.max(1, args.combo)) : map.combo,
+                hits,
+                miss: args.miss ? Math.min(hits, Math.max(0, args.miss)) : 0,
+                mods: new Mods(args.mods)
+            }, map.mode);
+            let pp = calc.calculate(ppArgs);
+            return `${map.artist} - ${map.title} [${map.version}] by ${map.creator.nickname}
 ${Util.formatBeatmapLength(map.length / calc.speedMultiplier)} | ${map.stats} ${Math.round(map.bpm * calc.speedMultiplier)}BPM | ${Util.round(map.diff.stars, 2)}✩ ${calc.mods.toString()}
 Точность: ${Util.round(ppArgs.acc * 100, 2)}%
 Комбо: ${Util.formatCombo(ppArgs.combo, map.combo)} | ${ppArgs.counts.miss} промахов
 - PP: ${Util.round(pp.pp, 2)}`;
+        }
+
+        case 3: {
+            let score = parseInt(args.string[0]);
+            let ppArgs = Util.createPPArgs({
+                hits: hits,
+                miss: 0,
+                score: isNaN(score) ? 1000000 : Math.min(1000000, Math.max(0, score)),
+                mods: new Mods(args.mods)
+            }, map.mode);
+            let pp = calc.calculate(ppArgs);
+            return `${map.artist} - ${map.title} [${map.version}] by ${map.creator.nickname}
+${Util.formatBeatmapLength(map.length / calc.speedMultiplier)} | ${map.stats} ${Math.round(map.bpm * calc.speedMultiplier)}BPM | ${Util.round(map.diff.stars, 2)}✩ ${calc.mods.toString()}
+Очки: ${ppArgs.score}
+- PP: ${Util.round(pp.pp, 2)}`;
+        }
+
+        default: 
+            return "Ошибка калькулятора";
+    }
 }
