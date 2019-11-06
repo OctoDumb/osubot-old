36bb6631e8c63fc980518c8c721bf8a371e391c2
uzervlad <vladik0701@gmail.com>
2019-11-06 20:35:29 +0300

    Full chat top in DM

--- src/modules/Bancho/Chat.ts
+++ src/modules/Bancho/Chat.ts
@@ -27,9 +27,39 @@ export default class BanchoChat extends Command {
                             return -1;
                         else return 0;
                     });
-                    if(users.length > 15)
-                        users = users.splice(0, 15);
-                    ctx.reply(`[Server: ${self.module.name}]\nТоп беседы:\n${users.map((user, i) => `#${i+1} ${user.nickname} | ${Util.round(user.pp, 1)} | Ранк ${user.rank} | ${Util.round(user.acc, 2)}%`).join("\n")}`);
+                    ctx.reply(`[Server: ${self.module.name}]\nТоп${users.length > 15 ? '-15' : ''} беседы (ID ${ctx.chatId}):\n${users.splice(0, 15).map((user, i) => `#${i+1} ${user.nickname} | ${Util.round(user.pp, 1)} | Ранк ${user.rank} | ${Util.round(user.acc, 2)}%`).join("\n")}`);
+                } catch(e) {
+                    console.log(e.toString());
+                    ctx.reply("Ошибка");
+                }
+            } else if(ctx.isFromUser) {
+                if(!args.string[0])
+                    return ctx.reply("Укажите ID беседы!");
+                let id = parseInt(args.string[0]);
+                if(isNaN(id))
+                    return ctx.reply("Некорректный ID!");
+                try {
+                    let members = (await self.module.bot.vk.api.messages.getConversationMembers({
+                        peer_id: id + 2000000000
+                    })).profiles;
+                    let ids = [];
+                    let users = [];
+                    for(let i = 0; i < members.length; i++) {
+                        let u = await self.module.bot.database.servers.bancho.getUser(members[i].id);
+                        if(u.nickname && !ids.includes(u.uid)) {
+                            users.push(u);
+                            ids.push(u.uid);
+                        }
+                    }
+                    users.filter(a => a.rank > 0 && a.pp > 0);
+                    users.sort((a,b) => {
+                        if(a.rank > b.rank)
+                            return 1;
+                        else if(a.rank < b.rank)
+                            return -1;
+                        else return 0;
+                    });
+                    ctx.reply(`[Server: ${self.module.name}]\nТоп беседы (ID ${id}):\n${users.map((user, i) => `#${i+1} ${user.nickname} | ${Util.round(user.pp, 1)} | Ранк ${user.rank} | ${Util.round(user.acc, 2)}%`).join("\n")}`);
                 } catch(e) {
                     console.log(e.toString());
                     ctx.reply("Ошибка");
