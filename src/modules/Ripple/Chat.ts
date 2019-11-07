9f4a750a5248e5a195dfa8e7c74f85f2f684b917
uzervlad <vladik0701@gmail.com>
2019-11-07 17:43:25 +0300

    Minor Chat fixes

--- src/modules/Ripple/Chat.ts
+++ src/modules/Ripple/Chat.ts
@@ -13,7 +13,6 @@ export default class RippleChat extends Command {
                     let users = [];
                     for(let i = 0; i < members.length; i++) {
                         let u = await self.module.bot.database.servers.ripple.getUser(members[i].id);
-                        users.some(uu => uu.uid == u.uid)
                         if(u.nickname && !users.some(uu => uu.uid == u.uid)) {
                             users.push(u);
                         }
@@ -26,12 +25,42 @@ export default class RippleChat extends Command {
                             return -1;
                         else return 0;
                     });
-                    if(users.length > 15)
-                        users = users.splice(0, 15);
-                    ctx.reply(`[Server: ${self.module.name}]\nТоп беседы:\n${users.map((user, i) => `#${i+1} ${user.nickname} | ${Util.round(user.pp, 1)} | Ранк ${user.rank} | ${Util.round(user.acc, 2)}%`).join("\n")}`);
+                    ctx.reply(`[Server: ${self.module.name}]\nТоп${users.length > 15 ? '-15' : ''} беседы (ID ${ctx.chatId}):\n${users.splice(0, 15).map((user, i) => `#${i+1} ${user.nickname} | ${Util.round(user.pp, 1)}pp | Ранк ${user.rank} | ${Util.round(user.acc, 2)}%`).join("\n")}`);
                 } catch(e) {
                     console.log(e.toString());
                     ctx.reply("Ошибка");
+                }
+            } else if(ctx.isFromUser) {
+                if(!args.string[0])
+                    return ctx.reply("Укажите ID беседы!");
+                let id = parseInt(args.string[0]);
+                if(isNaN(id))
+                    return ctx.reply("Некорректный ID!");
+                try {
+                    let members = (await self.module.bot.vk.api.messages.getConversationMembers({
+                        peer_id: id + 2000000000
+                    })).profiles;
+                    let ids = [];
+                    let users = [];
+                    for(let i = 0; i < members.length; i++) {
+                        let u = await self.module.bot.database.servers.ripple.getUser(members[i].id);
+                        if(u.nickname && !ids.includes(u.uid)) {
+                            users.push(u);
+                            ids.push(u.uid);
+                        }
+                    }
+                    users.filter(a => a.rank > 0 && a.pp > 0);
+                    users.sort((a,b) => {
+                        if(a.rank > b.rank)
+                            return 1;
+                        else if(a.rank < b.rank)
+                            return -1;
+                        else return 0;
+                    });
+                    ctx.reply(`[Server: ${self.module.name}]\nТоп беседы (ID ${id}):\n${users.map((user, i) => `#${i+1} ${user.nickname} | ${Util.round(user.pp, 1)}pp | Ранк ${user.rank} | ${Util.round(user.acc, 2)}%`).join("\n")}`);
+                } catch(e) {
+                    let err = await self.module.bot.database.errors.addError("b", ctx, String(e));
+                ctx.reply(`[Server: ${self.module.name}]\n${Util.error(String(e))} (${err})`);
                 }
             }
         });
