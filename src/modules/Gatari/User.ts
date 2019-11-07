8f0fd588f81bb0ed52d450e129c1db8f3ab4ae2a
uzervlad <vladik0701@gmail.com>
2019-11-07 19:37:07 +0300

    Gatari Donaters

--- src/modules/Gatari/User.ts
+++ src/modules/Gatari/User.ts
@@ -16,9 +16,10 @@ export default class GatariUser extends Command {
                 return ctx.reply("Не указан ник!");
             try {
                 let user = await self.module.bot.api.gatari.getUser(dbUser.nickname, dbUser.mode || 0);
+                let status = self.module.bot.donaters.status("gatari", user.id);
                 if(!dbUser.mode)
                     self.module.bot.database.servers.gatari.updateInfo(user);
-                ctx.reply(`[Server: ${self.module.name}]\n${self.module.bot.templates.User(user, dbUser.mode || 0, self.module.link)}`);
+                ctx.reply(`[Server: ${self.module.name}]\n${self.module.bot.templates.User(user, dbUser.mode || 0, status, self.module.link)}`);
             } catch (e) {
                 let err = await self.module.bot.database.errors.addError("g", ctx, String(e));
                 ctx.reply(`[Server: ${self.module.name}]\n${Util.error(String(e))} (${err})`);
