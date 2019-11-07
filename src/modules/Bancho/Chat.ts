1fb9c922243281dcc4784b18d6f0a67b19f7cb88
uzervlad <vladik0701@gmail.com>
2019-11-07 17:28:10 +0300

    Improve Chat command

--- src/modules/Bancho/Chat.ts
+++ src/modules/Bancho/Chat.ts
@@ -10,13 +10,11 @@ export default class BanchoChat extends Command {
                     let members = (await self.module.bot.vk.api.messages.getConversationMembers({
                         peer_id: ctx.peerId
                     })).profiles;
-                    let ids = [];
                     let users = [];
                     for(let i = 0; i < members.length; i++) {
                         let u = await self.module.bot.database.servers.bancho.getUser(members[i].id);
-                        if(u.nickname && !ids.includes(u.uid)) {
+                        if(u.nickname && !users.some(uu => uu.uid == u.uid)) {
                             users.push(u);
-                            ids.push(u.uid);
                         }
                     }
                     users.filter(a => a.rank > 0 && a.pp > 0);
--- src/modules/Gatari/Chat.ts
+++ src/modules/Gatari/Chat.ts
@@ -10,13 +10,11 @@ export default class GatariChat extends Command {
                     let members = (await self.module.bot.vk.api.messages.getConversationMembers({
                         peer_id: ctx.peerId
                     })).profiles;
-                    let ids = [];
                     let users = [];
                     for(let i = 0; i < members.length; i++) {
                         let u = await self.module.bot.database.servers.gatari.getUser(members[i].id);
-                        if(u.nickname && !ids.includes(u.uid)) {
+                        if(u.nickname && !users.some(uu => uu.uid == u.uid)) {
                             users.push(u);
-                            ids.push(u.uid);
                         }
                     }
                     users.filter(a => a.rank > 0 && a.pp > 0);
