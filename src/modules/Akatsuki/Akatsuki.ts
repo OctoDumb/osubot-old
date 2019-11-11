47ef22b4fa5640ab32b61e5842a3c5035756b5be
uzervlad <vladik0701@gmail.com>
2019-11-11 19:42:47 +0300

    Add missing Akatsuki commands

--- src/modules/Akatsuki/Akatsuki.ts
+++ src/modules/Akatsuki/Akatsuki.ts
@@ -4,6 +4,9 @@ import AkatsukiUser from "./User";
 import AkatsukiTop from "./Top";
 import AkatsukiNick from "./Nick";
 import AkatsukiMode from "./Mode";
+import AkatsukiChat from "./Chat";
+import AkatsukiFind from "./Find";
+import AkatsukiRecent from "./Recent";
 
 export default class Akatsuki extends Module {
     constructor(bot: Bot) {
@@ -17,7 +20,10 @@ export default class Akatsuki extends Module {
             new AkatsukiNick(this),
             new AkatsukiMode(this),
             new AkatsukiUser(this),
-            new AkatsukiTop(this)
+            new AkatsukiTop(this),
+            new AkatsukiChat(this),
+            new AkatsukiFind(this),
+            new AkatsukiRecent(this)
         ]);
     }
 }
