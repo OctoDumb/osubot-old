956a0e0a4386dc2e3f1bc9e1f80e68c3f35d6706
uzervlad <vladik0701@gmail.com>
2019-11-16 15:20:35 +0300

    Add progress to Recent

--- src/templates/Recent.ts
+++ src/templates/Recent.ts
@@ -4,6 +4,12 @@ import { IPPCalculator as ICalc } from '../pp/Calculator';
 
 export default function(score: APIRecentScore, beatmap: APIBeatmap, calc: ICalc, link: string) {
     let pp = calc.calculate(score);
+    let hits = beatmap.objects.circles + beatmap.objects.sliders + beatmap.objects.spinners;
+    if(score.mode == 1)
+        hits -= beatmap.objects.sliders;
+    if(score.mode == 1 || score.mode == 3)
+        hits -= beatmap.objects.spinners;
+    let progress = score.counts.totalHits() / hits;
     return `${beatmap.artist} - ${beatmap.title} [${beatmap.version}] by ${beatmap.creator.nickname}
 ${Util.formatBeatmapLength(beatmap.length / calc.speedMultiplier)} | ${beatmap.stats} ${Math.round(beatmap.bpm * calc.speedMultiplier)}BPM | ${Util.round(beatmap.diff.stars, 2)}✩ ${score.mods}
 
@@ -11,7 +17,7 @@ ${Util.formatBeatmapLength(beatmap.length / calc.speedMultiplier)} | ${beatmap.s
 Точность: ${Util.round(score.accuracy() * 100, 2)}%
 PP: ${pp.pp.toFixed(2)}${pp.ss == pp.pp ? '' : pp.fc == pp.pp ? ` ⯈ SS: ${pp.ss.toFixed(2)}` : ` ⯈ FC: ${pp.fc.toFixed(2)} ⯈ SS: ${pp.ss.toFixed(2)}`}
 Хиткаунты: ${score.counts.toString()}
-Ранк: ${score.rank}
+Ранк: ${score.rank} ${score.rank == "F" ? `(${Util.round(progress * 100, 2)}%)` : ''}
 
 Карта: ${link}/b/${beatmap.id.map}`;
 }
