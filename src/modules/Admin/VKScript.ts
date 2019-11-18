fc275e6520b1b7d60cf1962720263e9d3e4331be
uzervlad <vladik0701@gmail.com>
2019-11-18 15:13:00 +0300

    VKScript execution

---
+++ src/modules/Admin/VKScript.ts
@@ -0,0 +1,21 @@
+import { Command } from "../../Command";
+import { Module } from "../../Module";
+
+export default class AdminVKScript extends Command {
+    constructor(module: Module) {
+        super(["vks", "vkscript"], module, async (ctx, self, args) => {
+            if(ctx.senderId != module.bot.config.vk.owner) return;
+
+            let code = args.full.join(" ");
+            try {
+                let res = await self.module.bot.vk.api.execute({ code });
+                ctx.reply(
+                    typeof res == "object" ? 
+                    JSON.stringify(res) : (res || "null")
+                );
+            } catch(e) {
+                ctx.reply("Ошибка при выполнении кода!");
+            }
+        });
+    }
+}
