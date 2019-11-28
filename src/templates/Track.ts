463e463b84bbce6d0918d48967f7b8ce17513990
uzervlad <vladik0701@gmail.com>
2019-11-28 23:44:52 +0300

    Fix score place in Track template

--- src/templates/Track.ts
+++ src/templates/Track.ts
@@ -9,5 +9,5 @@ export default function(response: OsuTrackResponse): string {
 Rank: ${formatChange(response.rank)} (${formatChange(response.pp)} pp) in ${response.playcount} plays
 View detailed data here: https://ameobea.me/osutrack/user/${encodeURI(response.username)}
 
-${response.highscores.length == 0?'': (`${response.highscores.length} new highscores:\n`+response.highscores.slice(0, 3).map(score => `#${score.place} ${score.pp}pp https://osu.ppy.sh/b/${score.beatmapId}`).join("\n")) + (response.highscores.length > 3 ? ' and more..':'')}`;
+${response.highscores.length == 0?'': (`${response.highscores.length} new highscores:\n`+response.highscores.slice(0, 3).map(score => `#${score.place + 1} ${score.pp}pp https://osu.ppy.sh/b/${score.beatmapId}`).join("\n")) + (response.highscores.length > 3 ? ' and more..':'')}`;
 }
