5f71481a41f2f05c1c0c144fbd5047bb3e2282d0
uzervlad <vladik0701@gmail.com>
2019-12-01 14:40:00 +0300

    Fix News (add random_id)

--- src/News.ts
+++ src/News.ts
@@ -25,7 +25,7 @@ export default class News {
                 for(let j = 1; j <= 25; j++)
                     ids.push(2000000000 + (i*25) + j);
                 ids.filter(id => !this.forbidden.includes(id));
-                let code = ids.map(id => `API.messages.send(${JSON.stringify({peer_id: id, message, attachment: attachment || ""})});`).join("\n");
+                let code = ids.map(id => `API.messages.send(${JSON.stringify({peer_id: id, message, random_id: "a", attachment: attachment || ""})});`).join("\n");
                 await this.bot.vk.api.execute({ code });
             } catch(e) {
                 console.log(e);
