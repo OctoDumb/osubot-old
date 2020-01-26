77984233d9d3e47ef413dce1fe82b646795645a3
uzervlad <vladik0701@gmail.com>
2020-01-26 13:25:15 +0300

    Fix statuses in Leaderboard

--- src/templates/Leaderboard.ts
+++ src/templates/Leaderboard.ts
@@ -13,6 +13,6 @@ ${map.artist} - ${map.title} [${map.version}] by ${map.creator.nickname}\n` + le
         let smap = leaderboard.maps.find(m => m.mods == lbscore.score.mods.diff()).map;
         let calc = new BanchoPP(smap, lbscore.score.mods);
         let pp = calc.calculate(lbscore.score);
-        return `#${i+1} ${lbscore.user.nickname} ${getStatus(server.toLowerCase(), lbscore.user.id)} | ${lbscore.score.score} | ${Util.formatCombo(lbscore.score.combo, smap.combo)} | ${Util.round(lbscore.score.accuracy() * 100, 2)}% | ${Util.round(pp.pp, 2)}pp ${lbscore.score.mods} | ${Util.formatDate(lbscore.score.date, true)}`;
+        return `#${i+1} ${lbscore.user.nickname} ${getStatus(server.toLowerCase(), lbscore.user.uid)} | ${lbscore.score.score} | ${Util.formatCombo(lbscore.score.combo, smap.combo)} | ${Util.round(lbscore.score.accuracy() * 100, 2)}% | ${Util.round(pp.pp, 2)}pp ${lbscore.score.mods} | ${Util.formatDate(lbscore.score.date, true)}`;
     }).join("\n");
 }
