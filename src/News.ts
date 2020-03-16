b34f97acdde119bcb77bc26a3cb3f5836747037d
uzervlad <vladik0701@gmail.com>
2020-03-16 21:14:35 +0300

    News in DMs

--- src/News.ts
+++ src/News.ts
@@ -21,7 +21,8 @@ export default class News {
     bot: Bot;
     rules: INewsRules;
     limit: number;
-    defaults: INewsRule;
+    chatsDefaults: INewsRule;
+    usersDefaults: INewsRule;
     constructor(bot: Bot) {
         this.bot = bot;
         if(!fs.existsSync("./news_rules.json"))
@@ -29,8 +30,14 @@ export default class News {
         this.rules = JSON.parse(fs.readFileSync("./news_rules.json").toString());
         this.limit = 200;
 
-        this.defaults = {
+        this.chatsDefaults = {
             group: true,
+            osuupdate: false,
+            newranked: false
+        }
+
+        this.usersDefaults = {
+            group: false,
             osuupdate: false,
             newranked: false
         }
@@ -44,35 +51,69 @@ export default class News {
         let ids = [];
         for(let i = 1; i <= this.limit; i++)
             ids.push(2000000000 + i);
-        ids = ids.filter(id => this.allowed(id, options.type));
-        if(!ids.length) return;
-        for(let i = 0; i < Math.ceil(ids.length / 25); i++) {
-            try {
-                if(ids[0]) {
-                    let code = ids.splice(0, 25).map(id => `API.messages.send(${JSON.stringify({peer_id: id, message: options.message, random_id: 2281337, attachment: options.attachment || "", dont_parse_links: 1})});`).join("\n");
-                    await this.bot.vk.api.execute({ code });
+        ids = ids.filter(id => this.chatAllowed(id, options.type));
+        if(ids.length) {
+            for(let i = 0; i < Math.ceil(ids.length / 25); i++) {
+                try {
+                    if(ids[0]) {
+                        let code = ids.splice(0, 25).map(id => `API.messages.send(${JSON.stringify({peer_id: id, message: options.message, random_id: Math.ceil(Math.random() * 1e7), attachment: options.attachment || "", dont_parse_links: 1})});`).join("\n");
+                        this.bot.vk.api.execute({ code });
+                    }
+                } catch(e) {
+                    console.log(e);
                 }
-            } catch(e) {
-                console.log(e);
+            }
+        }
+        let rawUsers = (await this.bot.vk.api.execute({ code: 'var i = 0;\nvar users = [];\nwhile(i < 25) {\nvar u = API.messages.getConversations({"count": 200, "offset": 200 * i}).items@.conversation@.peer@.id;\nusers.push(u);\ni = i + 1;\nif(u.length < 200) {\ni = 25;\n}\n}\nreturn users;' })).response;
+        let users = [];
+        for(let i = 0; i < rawUsers.length; i++) {
+            users.push(...rawUsers[i]);
+        }
+        users = users.filter(u => this.userAllowed(u, options.type));
+        if(users.length) {
+            for(let i = 0; i < Math.ceil(users.length / 25); i++) {
+                try {
+                    if(users[0]) {
+                        let code = users.splice(0, 25).map(id => `API.messages.send(${JSON.stringify({peer_id: id, message: options.message, random_id: Math.ceil(Math.random() * 1e7), attachment: options.attachment || "", dont_parse_links: 1})});`).join("\n");
+                        this.bot.vk.api.execute({ code });
+                    }
+                } catch(e) {
+                    console.log(e);
+                }
             }
         }
         return true;
     }
 
     getChatRules(id: number): INewsRule {
-        return Object.assign({}, this.defaults, this.rules[id] || {});
+        return Object.assign({}, this.chatsDefaults, this.rules[id] || {});
+    }
+
+    getUserRules(id: number): INewsRule {
+        return Object.assign({}, this.usersDefaults, this.rules[id] || {});
     }
 
     save() {
         fs.writeFileSync("./news_rules.json", JSON.stringify(this.rules, null, "\t"));
     }
 
-    allowed(id: number, type: string): boolean {
+    chatAllowed(id: number, type: string): boolean {
         return this.getChatRules(id)[type];
     }
 
-    switch(id: number, type: string): boolean {
+    chatSwitch(id: number, type: string): boolean {
         let r = this.getChatRules(id);
+        this.rules[id] = r;
+        this.rules[id][type] = !r[type];
+        return this.rules[id][type];
+    }
+
+    userAllowed(id: number, type: string): boolean {
+        return this.getUserRules(id)[type];
+    }
+
+    userSwitch(id: number, type: string): boolean {
+        let r = this.getUserRules(id);
         this.rules[id] = r;
         this.rules[id][type] = !r[type];
         return this.rules[id][type];
--- src/modules/Main/News.ts
+++ src/modules/Main/News.ts
@@ -6,29 +6,40 @@ const types = ['group', 'osuupdate', 'newranked'];
 export default class NewsCommand extends Command {
     constructor(module: Module) {
         super("news", module, async (ctx, self, args) => {
-            if(!ctx.isChat)
-                return;
-            try {
-                let chat = (await self.module.bot.vk.api.messages.getConversationMembers({
-                    peer_id: ctx.peerId
-                }));
 
-                let user = chat.items.find(m => m.member_id == ctx.senderId);
-
-                if(!user.is_admin)
-                    return ctx.reply("Вы не можете управлять рассылкой!");
-                
+            if(ctx.isChat) {
+                try {
+                    let chat = (await self.module.bot.vk.api.messages.getConversationMembers({
+                        peer_id: ctx.peerId
+                    }));
+    
+                    let user = chat.items.find(m => m.member_id == ctx.senderId);
+    
+                    if(!user.is_admin)
+                        return ctx.reply("Вы не можете управлять рассылкой!");
+                    
+                    if(!args.string[0])
+                        return ctx.send(`Укажите тип рассылки! (${types.join("/")})`);
+    
+                    if(!types.includes(args.string[0].toLowerCase()))
+                        return ctx.send(`Неизвестный тип рассылки!`);
+    
+                    let n = self.module.bot.news.chatSwitch(ctx.peerId, args.string[0].toLowerCase());
+    
+                    ctx.reply(`Рассылка ${args.string[0].toLowerCase()} ${n ? 'включена' : 'отключена'}!`);
+                } catch(e) {
+                    ctx.reply("Мне нужны права администратора, чтобы проверить, являетесь ли Вы администратором!");
+                }
+            } else {
                 if(!args.string[0])
                     return ctx.send(`Укажите тип рассылки! (${types.join("/")})`);
 
                 if(!types.includes(args.string[0].toLowerCase()))
                     return ctx.send(`Неизвестный тип рассылки!`);
 
-                let n = self.module.bot.news.switch(ctx.peerId, args.string[0].toLowerCase());
+                let n = self.module.bot.news.userSwitch(ctx.peerId, args.string[0].toLowerCase());
 
                 ctx.reply(`Рассылка ${args.string[0].toLowerCase()} ${n ? 'включена' : 'отключена'}!`);
-            } catch(e) {
-                ctx.reply("Мне нужны права администратора, чтобы проверить, являетесь ли Вы администратором!");
             }
         });
     }
