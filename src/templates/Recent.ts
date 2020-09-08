87de9ded50514396a8af1b29edc834cf90a75ce3
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2020-09-08 18:59:19 +0300

    Change recent message format

--- src/templates/Recent.ts
+++ src/templates/Recent.ts
@@ -10,10 +10,9 @@ export default function(score: APIRecentScore, beatmap: APIBeatmap, calc: ICalc,
     if(score.mode == 1 || score.mode == 3)
         hits -= beatmap.objects.spinners;
     let progress = score.counts.totalHits() / hits;
-    return `${beatmap.artist} - ${beatmap.title} [${beatmap.version}] by ${beatmap.creator.nickname}
+    return ` <${beatmap.status}> ${beatmap.artist} - ${beatmap.title} [${beatmap.version}] by ${beatmap.creator.nickname}
 ${Util.formatBeatmapLength(beatmap.length / calc.speedMultiplier)} | ${beatmap.stats} ${Math.round(beatmap.bpm * calc.speedMultiplier)}BPM | ${Util.round(beatmap.diff.stars, 2)}✩ ${score.mods}
 
-Status: ${beatmap.status}
 Score: ${score.score} | Combo: ${Util.formatCombo(score.combo, beatmap.combo)}
 Accuracy: ${Util.round(score.accuracy() * 100, 2)}%
 PP: ${pp.pp.toFixed(2)}${pp.ss == pp.pp ? '' : pp.fc == pp.pp ? ` ⯈ SS: ${pp.ss.toFixed(2)}` : ` ⯈ FC: ${pp.fc.toFixed(2)} ⯈ SS: ${pp.ss.toFixed(2)}`}
