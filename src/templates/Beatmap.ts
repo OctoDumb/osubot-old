17dda1c8feae08528ad7350df77485a683e9ea93
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2020-12-29 21:47:22 +0300

    Add rebalance to beatmap command

--- src/templates/Beatmap.ts
+++ src/templates/Beatmap.ts
@@ -1,11 +1,13 @@
-import { APIBeatmap } from "../Types";
+import { APIBeatmap, BeatmapStatus } from "../Types";
 // import { IPPCalculator as ICalc } from "../pp/Calculator";
 import BanchoPP from "../pp/bancho";
 import Util from "../Util";
 import Mods from "../pp/Mods";
+import { BanchoStdRebalance } from "../pp/bancho/Calculator";
 
 export default function(map: APIBeatmap): string {
     let calc = new BanchoPP(map, new Mods(0));
+
     switch(map.mode) {
         case 0: { // osu!
             let pp98 = calc.calculate(Util.createPPArgs({
@@ -22,12 +24,34 @@ export default function(map: APIBeatmap): string {
                 miss: 0,
                 mods: new Mods(0)
             }, map.mode));
+
+            let rebalancePP98 = map.mode === 0 ? new BanchoStdRebalance(map, Util.createPPArgs({
+                acc: 0.98,
+                combo: map.combo,
+                hits: map.objects.circles + map.objects.sliders + map.objects.spinners,
+                miss: 0,
+                mods: new Mods(0)
+            }, map.mode)).value : null;
+            let rebalancePP99 = map.mode === 0 ? new BanchoStdRebalance(map, Util.createPPArgs({
+                acc: 0.99,
+                combo: map.combo,
+                hits: map.objects.circles + map.objects.sliders + map.objects.spinners,
+                miss: 0,
+                mods: new Mods(0)
+            }, map.mode)).value : null;
+
             return `${map.artist} - ${map.title} [${map.version}] by ${map.creator.nickname} (${map.status})
 ${Util.formatBeatmapLength(map.length)} | ${map.stats} ${Math.round(map.bpm)}BPM | ${Util.round(map.diff.stars, 2)}✩
 PP:
 - 98% = ${Util.round(pp98.pp, 2)}
 - 99% = ${Util.round(pp99.pp, 2)}
-- 100% = ${Util.round(pp98.ss, 2)}`;
+- 100% = ${Util.round(pp98.ss, 2)}
+${rebalancePP98 ? `Rebalance:
+- 98% = ${Util.round(rebalancePP98.pp, 2)}
+- 99% = ${Util.round(rebalancePP99.pp, 2)}
+- 100% = ${Util.round(rebalancePP98.ss, 2)}
+` : ""}
+`;
         }
 
         case 1: { // osu!taiko
