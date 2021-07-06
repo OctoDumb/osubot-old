9179b9f08ee8a3bcd3471123128e1f6835650e84
root <root@vladik0701.msk.network>
2021-07-06 21:22:17 +0000

    Fix playtime

--- src/templates/User.ts
+++ src/templates/User.ts
@@ -5,7 +5,7 @@ export default function(user: APIUser, mode: number, status: string, link: strin
     return `Player: ${user.nickname} ${status} (${Util.profileModes[mode]})
 Rank: #${user.rank.total} (${user.country} #${user.rank.country})
 Playcount: ${user.playcount} (Lv${Math.floor(user.level)})
-Playtime: ${Util.minutesToPlaytimeString(user.playcount)}
+Playtime: ${Util.minutesToPlaytimeString(user.playtime)}
 PP: ${Math.round(user.pp)}
 Accuracy: ${Util.round(user.accuracy, 2)}%
 
